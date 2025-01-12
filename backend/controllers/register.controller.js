d28d54b2e1b9f5512797ef3b8cb795a9a8443926
kamal-deep-131 <kamaldeepsinghsodhi2@gmail.com>
2025-01-12 11:49:17 +0530

    register user functionality

---
+++ backend/controllers/register.controller.js
@@ -0,0 +1,54 @@
+import User from "../model/user.model.js"
+import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
+
+const register = async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+
+        if (!name || !email || !password) {
+            return res.status(400).json({
+                success: false,
+                message: "All fields are required"
+            })
+        }
+
+        const userExists = await User.findOne({ email });
+
+        if (userExists) {
+            return res.status(400).json({
+                success: false,
+                message: "User already exists"
+            })
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const user = await User.create({
+            name,
+            email,
+            password: hashedPassword
+        })
+
+        await user.save();
+
+
+        const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET)
+
+        res.status(201).json({
+            success: true,
+            message: "User registered successfully",
+            token
+        })
+    }
+    catch (error) {
+        console.log("Error in register controller: ", error);
+        res.status(500).json({
+            success: false,
+            message: error.message
+        })
+    }
+
+}
+
+export default register
--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,12 +1,10 @@
 import { Router } from "express";
+import register from "../controllers/register.controller.js";
 
 const router = Router();
 
-router.get("/register", (req, res) => {
-    res.json({
-        message: "register"
-    });
-})
+router.post("/register", register)
+
 router.get("/login", (req, res) => {
     res.json({
         message: "login"
