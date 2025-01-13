dee18dfb62f596e7c9dea47e6873a624941feb93
kamal-deep-131 <kamaldeepsinghsodhi2@gmail.com>
2025-01-13 20:27:17 +0530

    register functionality from UI added

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react'
 import { InputField } from '../components'
-
-import { Link } from 'react-router-dom'
+import { toast } from "react-hot-toast"
+import { Link, useNavigate } from 'react-router-dom'
+import axios from "axios"
 
 
 const Register = () => {
@@ -12,12 +13,45 @@ const Register = () => {
     })
 
     const [loading, setLoading] = useState(false)
+    const navigate = useNavigate()
+    const baseUrl = import.meta.env.VITE_API_BASE_URL;
 
     const handleChange = (e) => {
         setFormData({ ...formData, [e.target.name]: e.target.value })
     }
 
-    const handleSubmit = () => { }
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        if (formData.name == "") {
+            toast.error("Please Enter Name")
+        } else if (formData.email == "") {
+            toast.error("Please Enter email")
+        }
+        else if (formData.password == "") {
+            toast.error("Please Enter password")
+        }
+        if (formData.name && formData.email && formData.password) {
+            try {
+                setLoading(true)
+                const response = await axios.post(`${baseUrl}/auth/register`, formData)
+                // localStorage.setItem("token", response?.data?.token)
+                console.log("token", response?.data?.token)
+                // localStorage.setItem("user", JSON.stringify(response?.data?.user))
+                console.log("user", JSON.stringify(response?.data?.user))
+                toast.success("Login Successful")
+                navigate("/")
+                setFormData({ name: "", email: "", password: "" })
+                setLoading(false)
+
+            } catch (error) {
+                setLoading(true)
+                toast.error(error?.response?.data?.message)
+                console.log(error)
+                setLoading(false)
+            }
+        }
+    }
+
     return (
         <section className='w-full h-full'>
             <div className='w-full  md:w-2/4 mx-auto my-8 p-6   rounded flex flex-col items-center justify-center gap-6 md:shadow-lg  md:border md:border-gray-600'>
